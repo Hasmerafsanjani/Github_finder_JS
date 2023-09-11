b1875b1e76cbce9a40c04e397984dade1f1f0112
Hasme Rafsanjani <hasmerafsanjani@gmail.com>
2023-09-12 00:40:24 +0530

    modified

--- script.js
+++ script.js
@@ -1,15 +1,72 @@
 console.log("script file linked");
 
-const form = document.getElementById("searchbar");
-  function getUsers(){
-    const api_url="https://api.github.com/users"
+const searchbar = document.getElementById("searchbar");
+const cardcontainer = document.getElementById("cardcontainer")
+const searchButton = document.getElementById("searchBtn")
+
+searchButton.addEventListener("click", (ele) => {
+    ele.preventDefault(); 
+    const searchValue = searchbar.value
+    getUsers(searchValue)
+})
+
+  function getUsers(searchValue){
+   let api_url
+
+    if(searchValue=== undefined){
+        api_url=`https://api.github.com/users`
+    }else{
+        api_url=`https://api.github.com/users/${searchValue}`
+    }
+
+    let result 
     const promise = fetch(api_url)
   console.log(promise);
 
   promise.then((resp)=>{
     return resp.json()
   }).then((data)=>{
-    console.log(data)
-  })
+    let result = data
+
+    if(searchValue===undefined){
+        result.map((ele) => {
+            const card = document.createElement("div")
+            const heading = document.createElement("h2")
+            const paragraph = document.createElement("p")
+            const img = document.createElement("img")
+            const link = document.createElement("a")
+              heading.innerText = ele.login
+               img.src = ele.avatar_url
+              link.href = ele.html_url
+              link.innerHTML= "Github Link"
+              card.appendChild(img)
+              card.appendChild(heading)
+              card.appendChild(link)
+              cardcontainer.appendChild(card)
+        })
+    }else{
+        cardcontainer.innerHTML="";
+         if(result.message === "Not Found"){
+            const heading = document.createElement("h1")
+            heading.innerText = "Nhi hai bhai yaha pe "
+            cardcontainer.appendChild(heading)
+         }else{
+
+             const card = document.createElement("div")
+             const heading = document.createElement("h2")
+             const paragraph = document.createElement("p")
+             const img = document.createElement("img")
+             const link = document.createElement("a")
+               heading.innerText = result.login
+                img.src = result.avatar_url
+               link.href = result.html_url
+               link.innerHTML= "Github Link"
+               card.appendChild(img)
+               card.appendChild(heading)
+               card.appendChild(link)
+               cardcontainer.appendChild(card)
+         }
+    }
+ })
 }
-  getUsers()
+  getUsers()  
